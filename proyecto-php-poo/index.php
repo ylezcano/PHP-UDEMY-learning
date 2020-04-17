4331b4052015ffdd298fd589b61af61f5dde9f0f
Yonny Lezcano <lezcano2535@hotmail.es>
2020-04-17 16:30:32 -0500

    listo script de base de datos

---
+++ proyecto-php-poo/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de camisetas</title>
+</head>
+<body>
+    <!-- CABECERA -->
+    <header id="header">
+        <div id="logo">
+            <img src="assets/img/camiseta.png" alt="camiseta logo">
+            <a href="index.php">
+                Tienda de camisetas
+            </a>
+        </div>
+    </header>
+    <!-- MENÚ -->
+
+    <!-- BARRA LATERAL -->
+
+    <!-- CONTENIDO CENTRAL -->
+
+    <!-- PIE DE PÁGINA -->
+</body>
+</html>
