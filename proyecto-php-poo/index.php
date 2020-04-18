b80a2cdae37098787b3da301603a7cae1cb19a2e
Yonny Lezcano <lezcano2535@hotmail.es>
2020-04-17 22:42:14 -0500

    lista la maquetación html y css del segundo proyecto

--- proyecto-php-poo/index.php
+++ proyecto-php-poo/index.php
@@ -1,26 +1,94 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de camisetas</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
 </head>
+
 <body>
-    <!-- CABECERA -->
-    <header id="header">
-        <div id="logo">
-            <img src="assets/img/camiseta.png" alt="camiseta logo">
-            <a href="index.php">
-                Tienda de camisetas
-            </a>
+    <div id="container">
+        <!-- CABECERA -->
+        <header id="header">
+            <div id="logo">
+                <img src="assets/img/camiseta.png" alt="camiseta logo">
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+        </header>
+        <!-- MENÚ -->
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="#">Inicio</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 1</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 2 </a>
+                </li>
+                <li>
+                    <a href="#">Categoria 3</a>
+                </li>
+                <li>
+                    <a href="#">Categoria 4</a>
+                </li>
+            </ul>
+        </nav>
+        <div id="content">
+            <!-- BARRA LATERAL -->
+            <aside id="lateral">
+                <div id="login" class="block_aside">
+                    <h3>Entrar a la web</h3>
+                    <form action="#" method="POST">
+                        <label for="email">Email</label>
+                        <input type="email" name="email">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password">
+                        <input type="submit" value="Enviar">
+                    </form>
+
+                    <ul>
+                        <li><a href="#">Mis pedidos</a></li>
+                        <li><a href="#">Gestionar pedidos</a></li>
+                        <li><a href="#">Gestionar productos</a></li>
+                    </ul>
+                </div>
+            </aside>
+
+            <!-- CONTENIDO CENTRAL -->
+            <div id="central">
+                <h1>Productos destacados</h1>
+
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button">Comprar</a>
+                </div>
+                <div class="product">
+                    <img src="assets/img/camiseta.png" alt="">
+                    <h2>Camiseta Azul Ancha</h2>
+                    <p>30 euros</p>
+                    <a href="" class="button">Comprar</a>
+                </div>
+            </div>
         </div>
-    </header>
-    <!-- MENÚ -->
-
-    <!-- BARRA LATERAL -->
-
-    <!-- CONTENIDO CENTRAL -->
-
-    <!-- PIE DE PÁGINA -->
+        <!-- PIE DE PÁGINA -->
+        <footer id="footer">
+            <p>Desarrollado por Yonny Lezcano WEB &copy; <?= date('Y') ?></p>
+        </footer>
+    </div>
 </body>
+
 </html>
